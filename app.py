d779ce7be6d15e0de90b4b3e9e05d4f38ca944db
sukhijashivam <151729042+sukhijashivam@users.noreply.github.com>
2025-11-08 22:25:32 +0530

    Update app.py
    Set all models in google drive

--- app.py
+++ app.py
@@ -22,7 +22,10 @@ os.makedirs(MODEL_DIR, exist_ok=True)
 # --- Download large models from Google Drive if missing ---
 drive_files = {
     "breast_cancer_model_last.h5": "12Y5ju8ZyAifCAHiQiqQ635eSnifxdTJt",  # ✅ Breast model
-    "Brain_Tumor_Classification_model.h5": "1TVhY0DEDbehA3A-t8GzbiSDY5G0LAlXc"  # ✅ Brain model
+    "Brain_Tumor_Classification_model.h5": "1TVhY0DEDbehA3A-t8GzbiSDY5G0LAlXc",  # ✅ Brain model
+    "diabetes_model.sav": "1yRAWrjY3B2K6s5X87ZdWduHChvgOu__V",
+    "heart_disease_model.sav": "12-9QP7AvEBHoEbbEEjFAG2JUkOYZwKJw",
+    "parkinsons_model.sav": "1sW9oZsmBVcpfPc0HVWozGpzgDasI1JWe"
 }
 #1TVhY0DEDbehA3A-t8GzbiSDY5G0LAlXc
 
