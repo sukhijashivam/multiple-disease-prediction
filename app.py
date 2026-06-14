8a546eef2203cb804eb05e6b234834c3943b6b66
sukhijashivam <151729042+sukhijashivam@users.noreply.github.com>
2026-06-14 12:04:48 +0530

    Update breast model filename in app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ for old_file in ["Brain_Tumor_Classification_model.h5", "breast_cancer_model_las
 #https://drive.google.com/file/d/1ZfgP53sorzuiSD3n0KbL3yAuG7uO1lyH/view?usp=sharing
 # --- Download large models from Google Drive if missing ---
 drive_files = {
-    "breast_v4.keras": "1Q0-2FYNhZiOS3VSV__xHbVgTAI_1XYLG",
+    "breast_fixed.keras": "1uzOr-U-kdDT1jjeXtAX8T9RS4_kTorfZ",
     "Brain_Tumor_Classification_model.keras": "1gZsxjKwNTeg--gp_pGi7HeAg1NOhqFD8",
     "diabetes_model.sav": "1yRAWrjY3B2K6s5X87ZdWduHChvgOu__V",
     "heart_disease_model.sav": "12-9QP7AvEBHoEbbEEjFAG2JUkOYZwKJw",
@@ -48,7 +48,7 @@ for name, file_id in drive_files.items():
 
 # --- Load models (after ensuring they're downloaded) ---
 
-breast_model = keras.models.load_model(os.path.join(MODEL_DIR, "breast_v4.keras"))
+breast_model = keras.models.load_model(os.path.join(MODEL_DIR, "breast_fixed.keras"))
 brain_model = keras.models.load_model(os.path.join(MODEL_DIR, "Brain_Tumor_Classification_model.keras"))
 diabetes_model = joblib.load(os.path.join(MODEL_DIR, "diabetes_model.sav"))
 heart_model = joblib.load(os.path.join(MODEL_DIR, "heart_disease_model.sav"))
